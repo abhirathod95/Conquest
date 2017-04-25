5d22e3c14e42d267bd40652f0a609c653b5b7dea
Safichka <daniel.zhang@live.com>
2017-04-24 23:34:28 -0500

    Add files via upload
    Harness that runs the nikto security scan command. Waiting until the website is up to begin parsing. -Daniel

---
+++ WebHack.py
@@ -0,0 +1,15 @@
+import sys
+from subprocess import call
+from bs4 import BeautifulSoup
+
+
+if len(sys.argv) < 2:
+    print('Pass at least a target domain to attack.')
+    sys.exit()
+
+# Takes a domain to run a nikto scan on; output is a .txt file. Script will wait until subprocess completes
+call('nikto -o test.txt -h ' + sys.argv[1], shell=True)
+
+with open('test.txt') as nikto_output:
+    for line in nikto_output:
+# gonna wait to see what the site looks like before parsing
