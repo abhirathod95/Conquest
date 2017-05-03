40c335bb1c89c0842de828148ca8bfd13e6a2235
Tanner <tannerjharper@gmail.com>
2017-05-03 16:07:05 -0500

    One flavor of SQLi found

--- ProbeWebsite.py
+++ ProbeWebsite.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 
 vulnerabilities = {}
 vulnerabilityCounter = 0
+loginPage = []
 
 def examineJavascriptForXSS(html, targetURL, inputField, session):
 	return False  # temp value until function is actually implemented
@@ -26,11 +27,11 @@ def locateScriptTagsWithString(html, stringToFind) :
 				# find out if this element has script tags associated with it
 	return
 
-def probeFoundXSSVulnerability(html, targetURL, inputField, session):
+def probeFoundXSSVulnerability(html, targetURL, inputField, session) :
 	randomNumber = random.randint(5599993912, 2459510340230403407)
 	baseString ="I really really x" + str(randomNumber) + " times like this"
 	probeText = baseString + "<script></script>"
-	data = {"body" : probeText, "placeholder" : probeText, "text" : probeText}
+	data = {"body" : probeText, "placeholder" : probeText, "text" : probeText, "user" : probeText, "username" : probeText, "password": probeText, "pass" : probeText, "pw" : probeText}
 	try :
 		session.post(targetURL, data)
 		html = session.get(targetURL)
@@ -41,7 +42,43 @@ def probeFoundXSSVulnerability(html, targetURL, inputField, session):
 	except :
 		return
 
+def checkIfSQLi(html) :
+	htmlPage = html.prettify()
+	error = "error" in htmlPage or "ERROR" in htmlPage or "Error" in htmlPage or "err" in htmlPage or "ERR" in htmlPage or "Err" in htmlPage
+	sql = "sql" in htmlPage or "SQL" in htmlPage or "Sql" in htmlPage
+	grammar = "syntax" in htmlPage or "Syntax" in htmlPage or "SYNTAX" in htmlPage or "statement" in htmlPage or "Statement" in htmlPage or "STATEMENT" in htmlPage
 
+	if(error and sql and grammar) :				# checks for defacto SQLi information leak. If true, then there's a SQL vulnerability.
+		return True
+
+	return False
+
+def probeFoundSQLiVulnerability(html, targetURL, inputField, session) :
+	probeText1 = "Wayne's World"
+	data = {"username" : "'", "password" : "'", "name" : "'", "firstname" : "'", "first" : "'", "last" : "'", "fname" : "'", "lname" : "'"}
+
+	try :
+		probeText = probeText1
+		html = session.post(targetURL, data)
+		if("405" in html.text or "403" in html.text or "400" in html.text) :
+			raise Exception
+		html = BeautifulSoup(html.text, "html.parser")
+		vulnerable = checkIfSQLi(html)
+		if(vulnerable) :
+			return probeText
+	except :
+		try :
+			probeText = probeText1
+			name = inputField.attrs['name']
+			queryURL = targetURL + "?" + name + "=" + probeText
+			html = session.get(queryURL)
+			html = BeautifulSoup(html.text, "html.parser")
+			vulnerable = checkIfSQLi(html)
+			if(vulnerable) :
+				return probeText
+		except :
+			return
+	return
 
 def examineForVulnerabilities(html, targetURL, element, session):
 	global vulnerabilityCounter
@@ -53,19 +90,24 @@ def examineForVulnerabilities(html, targetURL, element, session):
 		if (jsIsVulnerableToXSS):
 			pageExploits["XSS" + str(vulnerabilityCounter)] = str(element)
 			vulnerabilityCounter += 1
-			print(
-			"XSS VULNERABILITY FOUND: " + str(element) + " on page " + targetURL + ". Javascript validation is broken.\n")
+			print("XSS VULNERABILITY FOUND: " + str(element) + " on page " + targetURL + " . Javascript validation is broken.\n")
 		else:
 			print("No XSS vulnerabilities immediately found for " + str(element) + " on page " + targetURL + "\nEither element is not vulnerable, or website is good about not leaking information.\n	")
 	else:
 		print("Probing with sample input to test for XSS vulnerabilities\n")
 		isVulnerable = probeFoundXSSVulnerability(html, targetURL, element, session)
-		if (isVulnerable):
+		if (isVulnerable) :
 			pageExploits["XSS" + str(vulnerabilityCounter)] = str(element)
 			vulnerabilityCounter += 1
-			print("XSS VULERABILITY FOUND: \" " + str(element) + " \" on page " + targetURL + ". Found using probe-text: \" " + isVulnerable + " \"\n")
+			print("XSS VULERABILITY FOUND: \" " + str(element) + " \" on page " + targetURL + " . Found using probe-text: \" " + isVulnerable + " \"\n")
 		else:
 			print("No XSS vulnerabilities immediately found for " + str(element) + " on page " + targetURL + "\nEither element is not vulnerable, or website is good about not leaking information.\n")
+			print("Probing with sample input to test for SQLi vulnerabilities\n")
+			isVulnerable = probeFoundSQLiVulnerability(html, targetURL, element,	session)
+			if(isVulnerable) :
+				pageExploits["SQLi" + str(vulnerabilityCounter)] = str(element)
+				vulnerabilityCounter += 1
+				print("SQLi VULNERABILITY FOUND: \" " + str(element) + "\" on page " + targetURL + " . Found using probe-text: \" " + isVulnerable + " \"\n")
 
 	return pageExploits
 
@@ -85,6 +127,8 @@ def probeTheWebsite(baseURL="http://127.0.0.1:5000", targetPage=['/', '/login', 
 		print("Passed in an unauthenticated session. Trying all pages that don't require authentication. Results may be LIMITED.\n")
 	for restOfURL in targetPage :
 		targetURL = baseURL + restOfURL
+		if("login" in restOfURL) :
+			loginPage.append(targetURL)
 		currentPageExploits = {}
 		try :
 			html = session.get(targetURL)
@@ -121,4 +165,4 @@ def probeTheWebsite(baseURL="http://127.0.0.1:5000", targetPage=['/', '/login', 
 			print("1 vulnerability found!\n")
 		else :
 			print(str(vulnerabilityCounter) + " vulnerabilities found!\n")
-
+	#
