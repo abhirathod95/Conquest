b6f84e1eff19fa11f18d4adec814dffc33288165
abhirathod95 <abhirathod95@gmail.com>
2017-05-03 14:59:56 -0500

    Added vuln page

--- prompt.py
+++ prompt.py
@@ -5,7 +5,12 @@ import sys
 def getInput(login,vulnerabilities,session):
 	exploits = ["SQL Injection","Cross Site Scripting","Something else"]
 	payloads = [["Query 1","Query 2","Query 3"],["Steal user credentials using fake login page","b","c"],["1","2","3"]]
+	print("Please select the pages you would like to exploit:")
+	print("{:<30} {:<30}".format("Pages", "Vulnerabilities"))
+	for page in vulnerabilities:
+		print("{:<30} {:<30}".format(page[0], ",".join(page[1])))
 	print("Type the number next to the type of exploit you want")
+
 	count = 1
 	for i in exploits:
 		print( str(count) + " : "+ i)
@@ -27,7 +32,8 @@ def getInput(login,vulnerabilities,session):
 		r = requests.get(login)
 	"""
 
-
+if __name__ == "__main__":
+	getInput(None, [("/login", [])])
 
 
 """
---
+++ vuln_page.py
@@ -0,0 +1,13 @@
+class VulnPage:
+
+	def __init__(self, name, url, vuln, data):
+		self.name = name
+		self.url = url
+		self.vuln = vuln
+		self.data = data
+
+	def __str__(self):
+		return "Name: {} URL: {} Vuln: {}".format(self.name, self.url, self.pprint_vuln())
+
+	def pprint_vuln(self):
+		return ",".join(self.vuln)
